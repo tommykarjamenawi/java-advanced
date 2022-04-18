36700918a37320115f6ca29df5fec1e40e9a5c8b
Tommy Karjamenawi <54915266+tommykarjamenawi@users.noreply.github.com>
2022-04-18 18:37:05 +0200

    rename controller

--- src/main/java/nl/inholland/carAPI/controller/CarController.java
+++ src/main/java/nl/inholland/carAPI/controller/CarController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class DemoController {
+public class CarController {
 
     @Autowired
     CarService carService;
