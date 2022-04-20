1f0b67a9424bdc8017ae4d14f1c6846829243b12
Tommy Karjamenawi <54915266+tommykarjamenawi@users.noreply.github.com>
2022-04-20 02:52:25 +0200

    updated according to new lectures

---
+++ src/main/java/nl/inholland/carapi/CarApiApplication.java
@@ -0,0 +1,35 @@
+package nl.inholland.carapi;
+
+import nl.inholland.carapi.model.Car;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class CarApiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(CarApiApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+
+	}
+
+	@Bean
+	public List<Car> cars() {
+		// returns list of cars
+		return new ArrayList<>((List.of(
+				new Car(1, "Ferrari", "F40", 200000, 1988),
+				new Car(2, "Tesla", "S", 70000, 2019),
+				new Car(3, "Volvo", "V-50", 20000, 1988),
+				new Car(4, "Toyota", "Andrada", 15000, 2020),
+				new Car(5, "Honda", "H37", 25000, 2007)
+		)));
+	}
+
+}
---
+++ src/main/java/nl/inholland/carapi/model/Car.java
@@ -0,0 +1,70 @@
+package nl.inholland.carapi.model;
+
+
+public class Car {
+
+    private long id;
+    private String brand;
+    private String model;
+    private double price;
+    private int year;
+
+    public Car(long id, String brand, String model, double price, int year) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.year = year;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                '}';
+    }
+}
